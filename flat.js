a58505a86aed5dd0a98bff271fb9367806894ac0
Alex Platt <alja.platt@gmail.com>
2021-12-31 17:24:03 +0000

    flat

--- flat.js
+++ flat.js
@@ -1,3 +1,16 @@
 /* FLAT 
--
+- creats a new array, with all the sub arrays concatenated into it 
 */
+
+// const arr = [1, [2, [3, [4]]]];
+
+// [1,2,3,4]
+
+// const res = arr.flat();
+
+// default is 1 layer
+
+const arr = [1, [2, [3, [4]]]];
+// const res = arr.flat(3);
+const res = arr.flat(Infinity);
+console.log(res); // [1,2,3,4]
--- isArray.js
+++ isArray.js
@@ -1,3 +1,10 @@
 /* IS ARRAY
--
+- checks if the passed value is an array
+- lives on the Array class 
 */
+
+const arr = ["Florin", "Ivan", "Liam"];
+const str = "hello world";
+const number = 7;
+
+console.log(Array.isArray(arr)); // returns true
